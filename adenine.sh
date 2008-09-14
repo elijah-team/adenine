30e24f340fed6e88d65a8ea1cbd240bc7ab0fe66
jimwhite <jimwhite@c0584dae-7634-0410-9af6-c88a1969ce78>
2008-09-14 19:47:40 +0000

    Trying to make directly runnable jar, but not quite there.
    git-svn-id: svn://svn.code.sf.net/p/ifcx/code/thirdparty/adenine@137 c0584dae-7634-0410-9af6-c88a1969ce78

--- adenine.sh
+++ adenine.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+#export CLASSPATH=`find subfloor/jars -name "*jar" | paste -sd ':' -`
+#echo $CLASSPATH
+#java -jar adenine.jar
 java -cp adenine.jar:`find subfloor/jars -name "*jar" | paste -sd ':' -` edu.mit.lcs.haystack.adenine.Console
